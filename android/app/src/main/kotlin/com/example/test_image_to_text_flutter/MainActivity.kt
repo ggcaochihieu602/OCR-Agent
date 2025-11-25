09c63a7a69b1ab593f7214880bef1634cc610c84
24neurons <momo123vmf@gmail.com>
2025-11-25 23:15:48 +0700

    Initialized project

---
+++ android/app/src/main/kotlin/com/example/test_image_to_text_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_image_to_text_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,269 @@
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? _imageFile;
+  Future<void> _pickerImage() async {
+    final pickedFile = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+      _processImage();
+    }
+  }
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  Future<void> _processImage() async {
+    final inputImage = InputImage.fromFilePath(_imageFile!.path);
+    final textRecognizer = TextRecognizer();
+    final RecognizedText recognizedText = await textRecognizer.processImage(
+      inputImage,
+    );
+    String extractedText = recognizedText.text;
+    print(extractedText);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _imageFile == null
+                  ? Text('No image selected.')
+                  : Image.file(_imageFile!),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _pickerImage,
+                child: Text('Select Image'),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// Camera opening
+// import 'dart:async';
+// import 'dart:io';
+
+// import 'package:camera/camera.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+// Future<void> main() async {
+//   // Ensure that plugin services are initialized so that `availableCameras()`
+//   // can be called before `runApp()`
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   // Obtain a list of the available cameras on the device.
+//   final cameras = await availableCameras();
+
+//   // Get a specific camera from the list of available cameras.
+//   final firstCamera = cameras.first;
+
+//   runApp(
+//     MaterialApp(
+//       theme: ThemeData.dark(),
+//       home: TakePictureScreen(
+//         // Pass the appropriate camera to the TakePictureScreen widget.
+//         camera: firstCamera,
+//       ),
+//     ),
+//   );
+// }
+
+// // A screen that allows users to take a picture using a given camera.
+// class TakePictureScreen extends StatefulWidget {
+//   const TakePictureScreen({super.key, required this.camera});
+
+//   final CameraDescription camera;
+
+//   @override
+//   TakePictureScreenState createState() => TakePictureScreenState();
+// }
+
+// class TakePictureScreenState extends State<TakePictureScreen> {
+//   late CameraController _controller;
+//   late Future<void> _initializeControllerFuture;
+//   final textRecognizer = TextRecognizer();
+//   final Recog
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     // To display the current output from the Camera,
+//     // create a CameraController.
+//     _controller = CameraController(
+//       // Get a specific camera from the list of available cameras.
+//       widget.camera,
+//       // Define the resolution to use.
+//       ResolutionPreset.medium,
+//     );
+
+//     // Next, initialize the controller. This returns a Future.
+//     _initializeControllerFuture = _controller.initialize();
+//   }
+
+//   @override
+//   void dispose() {
+//     // Dispose of the controller when the widget is disposed.
+//     _controller.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Take a picture')),
+//       // You must wait until the controller is initialized before displaying the
+//       // camera preview. Use a FutureBuilder to display a loading spinner until the
+//       // controller has finished initializing.
+//       body: FutureBuilder<void>(
+//         future: _initializeControllerFuture,
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.done) {
+//             // If the Future is complete, display the preview.
+//             return CameraPreview(_controller);
+//           } else {
+//             // Otherwise, display a loading indicator.
+//             return const Center(child: CircularProgressIndicator());
+//           }
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         // Provide an onPressed callback.
+//         onPressed: () async {
+//           // Take the Picture in a try / catch block. If anything goes wrong,
+//           // catch the error.
+//           try {
+//             // Ensure that the camera is initialized.
+//             await _initializeControllerFuture;
+
+//             // Attempt to take a picture and get the file `image`
+//             // where it was saved.
+//             final image = await _controller.takePicture();
+
+//             if (!context.mounted) return;
+
+//             // If the picture was taken, display it on a new screen.
+//             await Navigator.of(context).push(
+//               MaterialPageRoute<void>(
+//                 builder: (context) => DisplayPictureScreen(
+//                   // Pass the automatically generated path to
+//                   // the DisplayPictureScreen widget.
+//                   imagePath: image.path,
+//                 ),
+//               ),
+//             );
+//           } catch (e) {
+//             // If an error occurs, log the error to the console.
+//             print(e);
+//           }
+//         },
+//         child: const Icon(Icons.camera_alt),
+//       ),
+//     );
+//   }
+// }
+
+// // A widget that displays the picture taken by the user.
+// class DisplayPictureScreen extends StatelessWidget {
+//   final String imagePath;
+
+//   const DisplayPictureScreen({super.key, required this.imagePath});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Display the Picture')),
+//       // The image is stored as a file on the device. Use the `Image.file`
+//       // constructor with the given path to display the image.
+//       body: Image.file(File(imagePath)),
+//     );
+//   }
+// }
